889151fef53f682b12ed143aa01640e0b742ece0
Marlos11 <marlos-gama@hotmail.com>
2023-09-21 17:49:31 -0300

    altercao em alguns estilos e inicio da responsividade

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
+/* ".swiper-button-prev"
+.swiper-button-next", */
 
-let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-   
-})
+
+let swiper = createSwiper(".mySwiper",".swiper-pagination",".swiper-button-next",".swiper-button-prev")
+
+function createSwiper  (container,pagination,nextButton,prevButton) {
+    return new Swiper(container, {
+        slidesPerView: handleWidth(),
+        spaceBetween: 30,
+        pagination: {
+            el: pagination,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: nextButton,
+            prevEl: prevButton,
+        },
+    
+    })
+}
+
+ 
 
 
 let header = document.getElementById('header')
 
-window.addEventListener('scroll', () =>{
-    if(window.scrollY >=200){
+function  handleWidth  (){
+    let getWidth = window.innerWidth || document.documentElement.clientWidth
+    let slideShow = 3 
+    
+
+    if(getWidth < 1001 ){
+        slideShow = 2 
+    }
+
+    if(getWidth <700){
+        slideShow = 1 
+    }
+
+    return slideShow
+}
+
+window.addEventListener('resize', ()=>{
+    swiper.params.slidesPerView = handleWidth()
+    swiper.update()
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 200) {
         header.style.background = '#191919'
-    }else{
+    } else {
         header.style.background = 'transparent'
     }
-})
+}) 
+
